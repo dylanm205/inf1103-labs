36cf2ebd72e23ce87dd83345afa109747c8bb1ad
Dylan <2601885@sit.singaporetech.edu.sg>
2026-09-14 01:21:11 +0800

    Enforce Business Rules

--- Week 2/auditor.py
+++ Week 2/auditor.py
@@ -4,7 +4,12 @@ while True:
     if user_input.lower() == 'quit':
         break
     else:
+        if "-"  in user_input:
+            if user_input.strip("-").isdigit() == True:
+                print("Invalid input. Please enter a non-negative stock quantity.")
+                continue
         if user_input.isdigit() == False:
             print("Invalid input. Please enter a valid stock quantity.")
             continue
         user_input = int(user_input)
+
