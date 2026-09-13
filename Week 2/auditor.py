2dcd5adab04ef61c4a4eca3621381fff03c5c5dc
Dylan <2601885@sit.singaporetech.edu.sg>
2026-09-14 00:01:33 +0800

    Accept stock values as integers

--- Week 2/auditor.py
+++ Week 2/auditor.py
@@ -3,4 +3,6 @@ while True:
     user_input = input("Enter stock quantity (or type 'quit' to exit): ")
     if user_input.lower() == 'quit':
         break
-    
+    else:
+        user_input = int(user_input)
+        
